2c853cbd8578f90ab3255bbbba6e12552abfe0d0
Victor Perdiguer <victor.perdiguer@gmail.com>
2022-11-15 20:44:45 +0100

    iterations 1-5 done

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 /* Your code goes here */
 
-// Iteration 2 tests:
-// console.log(getMaxNum([45, 67, 12, 34])) // Should return 67
-// console.log(getMaxNum(['hello', [0, 1, 2], 34, 45, 12, 0])) // Should return 45
-
+function getMaxNum(list) {
+    return Math.max(...list.filter(elem => typeof elem === 'number'));
+}
+console.log(getMaxNum([45, 67, 12, 34])) // Should return 67
+console.log(getMaxNum(['hello', [0, 1, 2], 34, 45, 12, 0])) // Should return 45
 
 
 
@@ -14,65 +15,64 @@
 /* Your code goes here */
 
 // Iteration 3 tests:
-// console.log(changeItem([3, 5, 7], 3, 4)) // Should return [4,5,7]
-// console.log(changeItem([17, 18, 6, 18, 3], 18, 2)) // Should return [17, 2, 6, 2, 3];
-// console.log(changeItem([1, 1, 3, 2, 4, 1, 1], 1, 8)) // Should return [8, 8, 3, 2, 4, 8, 8] 
-
+function changeItem(list, firstNum, secondNum) {
+    for (let i = 0; i<list.length; i++) {
+        if (list[i] === firstNum) {
+            list[i] = secondNum;
+        }
+    }
+    return list;
+}
+console.log(changeItem([3, 5, 7], 3, 4)) // Should return [4,5,7]
+console.log(changeItem([17, 18, 6, 18, 3], 18, 2)) // Should return [17, 2, 6, 2, 3];
+console.log(changeItem([1, 1, 3, 2, 4, 1, 1], 1, 8)) // Should return [8, 8, 3, 2, 4, 8, 8]
 
 
 
 /// Iteration 4: orderByDate
-
-/* Your code goes here */
-
-// Iteration 4 tests:
-// const birthdays = [
-//   {
-//     name: 'Bob',
-//     year: 1989,
-//   },
-//   {
-//     name: 'Rosita',
-//     year: 1990,
-//   },
-//   {
-//     name: 'Stevie',
-//     year: 1988,
-//   },
-//   {
-//     name: 'Marlon',
-//     year: 1979,
-//   },
-// ]
-// const importantDates = [
-//   {
-//     title: 'Wedding aniversary',
-//     year: 2014,
-//   },
-//   {
-//     name: "Daddy's birthday",
-//     year: 2013,
-//   },
-//   {
-//     name: 'Graduation party',
-//     year: 2019,
-//   }
-// ]
-// console.log(orderByDate(birthdays)) // Should return them ordered 1979, 1988, 1989, 1990
-// console.log(orderByDate(importantDates)) // Should return them ordered 2013, 2014, 2019
-
+function orderByDate (birthdays) {
+    return birthdays.sort((a, b) => a.year - b.year);
+}
+console.log(orderByDate(birthdays)) // Should return them ordered 1979, 1988, 1989, 1990
+console.log(orderByDate(importantDates)) // Should return them ordered 2013, 2014, 2019
 
 
 
 /// Iteration 5: new machine gun
 
 /* Your code goes here */
+class Weapon {
+    constructor(type, power, ammo = 10) {
+        this.type = type,
+        this.power = power,
+        this.ammo = ammo,
+        this.shootingInterval = undefined
+    }
 
+    shoot() {
+        this.shootingInterval = setInterval(() => {
+            this.ammo--;
+            console.log("pewpew!", "ammo left:", this.ammo);
+        }, 30)
+    }
+
+    stopShooting(){
+        clearInterval(this.shootingInterval);
+        console.log("HOLD YOUR FIRE!");
+    }
+
+    reload(bullets){
+        setTimeout(() => {
+            this.ammo += bullets;
+            console.log("Reload!", "Total ammo:", this.ammo);
+        }, 2000);
+    }
+}
 // Iteration 5 tests
-// const machineGun = new Weapon('Machine gun', 90, 50);
-// console.log('Begin:')
-// machineGun.shoot();
-// setTimeout(() => machineGun.stopShooting(), 1000);
-// setTimeout(() => machineGun.reload(35), 1500);
+const machineGun = new Weapon('Machine gun', 90, 50);
+console.log('Begin:')
+machineGun.shoot();
+setTimeout(() => machineGun.stopShooting(), 1000);
+setTimeout(() => machineGun.reload(35), 1500);
 
 
